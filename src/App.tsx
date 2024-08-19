ac1daff7d3702ca4e775f2522bf4e020c618e42d
gnyatsikor <fannynyatsikor@gmail.com>
2024-08-19 05:56:55 +0200

    add Info element

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,7 @@ import Blog from "./components/Blog"
 
 function App() {
   
-
   return (
-
     
    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
     <div className=' top-0 -z-10 h-full w-full'>
--- src/data/info.ts
+++ src/data/info.ts
@@ -5,12 +5,35 @@ export const ABOUT_ME = "Curious about how systems work, I am an autonomous and 
 
 export const EXPERIENCE = [
     {
-
+        year : "2024",
+        role : "Android developper Intern",
+        company : "ATEME",
+        c_Logo : "",
+        description:  "Develop a Multicast gateway Adaptive Bitrate(ABR) Streaming on an android platform",
+        technologies : ["Kotlin", "Git", "GitLab", "C++", "JNI", "Jetpack Compose"]
     },
     {
-
+        year : "2023-2024",
+        role : "Part-time research Engineer",
+        company : "IRISA",
+        c_Logo : "",
+        description:  "Extending my contributions to the BMDEC project  which aims to design a decision-support tool: Explore identity and Access Management tool",
+        technologies : ["Python", "Flask", "Git", "Gitlab", "Keycloack", "Docker"]
     },
     {
-
+        year : "2023",
+        role : "Research Engineer Intern",
+        company : "IRISA",
+        c_Logo : "",
+        description:  "Contributed to a research project(BMDEC), focused on analyzing transport user behavior to design decision support tool.Developed a web application for visualization and editing collected experimental  data ",
+        technologies : ["Python", "Flask", "Git", "Gitlab"]
+    },
+    {
+        year : "2022",
+        role : " Web development Intern",
+        company : "FAST",
+        c_Logo : "",
+        description:  "Develop a web service  added to the administrator web app, enabling the marketing team to generate user profiles",
+        technologies : ["C#", ".Net", "Angular","TypeScript", "Git","Fork" ]
     },
 ]
