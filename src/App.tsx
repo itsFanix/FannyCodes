92f2abf3cf8c11554a2cd277bf7067695ea302a1
fanny <renatenyatsikor@gmail.com>
2025-10-31 23:58:30 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,28 @@
-import {BrowserRouter as Router,  Route , Routes} from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Home from "./components/Home"
-import Experiences from "./components/Experiences"
-import Project from "./components/Project"
-import Blog from "./components/Blog"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Experiences from "./components/Experiences";
+import Project from "./components/Project";
+import Blog from "./components/Blog";
 //import Footer from "./components/Footer"
 
-
-
 function App() {
-  
+  //  text-neutral-100 overflow-x-hidden antialiased selection:bg-cyan-300 selection:text-cyan-900
   return (
-    
-   <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
-    <div className=' top-0 -z-10 h-full w-full'>
-  
-    <div className="fixed inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(135%_135%_at_50%_10%,#1e293b_50%,#10b981_60%,#63e_100%)]">
+    <div className="">
+      {/* <div className="fixed inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(135%_135%_at_50%_10%,#1e293b_50%,#10b981_60%,#63e_100%)]"></div> */}
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Experiences" element={<Experiences />} />
+          <Route path="/Project" element={<Project />} />
+          <Route path="/Blog" element={<Blog />} />
+        </Routes>
+      </Router>
+      {/* <Footer/> */}
     </div>
-
-    <div className="container ">
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element= {<Home/>}/>
-        <Route path="/Experiences" element= {<Experiences/>}/>
-        <Route path="/Project" element= {<Project/>}/>
-        <Route path="/Blog" element= {<Blog/>}/>
-      </Routes>
-    </Router>
-    </div>
-   
-    </div>
-  {/* <Footer/> */}
-   </div>
-   
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,83 +1,80 @@
-
-import { FaLinkedin } from 'react-icons/fa'
-import { FaGithub } from 'react-icons/fa'
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 // import { FaInstagram } from 'react-icons/fa'
 
-
-import proficPic from "../assets/fotor-Fai.jpg"
-import { ABOUT_ME } from '../data/info'
-import { motion } from 'framer-motion'
+import proficPic from "../assets/fotor-Fai.jpg";
+import { ABOUT_ME } from "../data/info";
+import { motion } from "framer-motion";
 
 const container = (delay: number) => ({
   hidden: { x: -100, opacity: 0 },
-  visible: { x: 0, opacity: 1, transition: { duration: 0.5, delay: delay } }
-})
+  visible: { x: 0, opacity: 1, transition: { duration: 0.5, delay: delay } },
+});
 
 function Home() {
   return (
     // <section className='mx-auto lg:mt-20 2xl:mt-30 2xl:mb-28'>
+    <div>
+      {/* className='w-full lg:w-1/2' */}
       <div>
-        {/* className='w-full lg:w-1/2' */}
-        <div >
         {/* className='flex p-2 flex-col items-center lg:items-start' */}
-          <div >
-
-            <motion.h1
-              variants={container(0)}
-              initial="hidden"
-              animate="visible"
-              className='pb-4 text-5xl font-thin tracking-tight lg:mt-6 lg:text-6xl 2xl:text-7xl'>
-              Fanny NYATSIKOR
-            </motion.h1>
-            <motion.span
-              variants={container(0)}
-              initial="hidden"
-              animate="visible"
-              className='bg-gradient-to-r from-green-300 via-blue-400 to-purple-500 bg-clip-text text-2xl tracking-tight font-thin text-transparent'>
-              Sotfware Engineer - Language Enthusiast
-            </motion.span>
-
-
-            <motion.p
-              variants={container(0)}
-              initial="hidden"
-              animate="visible"
-              className='my-2 max-w-xl py-4 font-light tracking-tighter text-justify'>
-              {ABOUT_ME}
-            </motion.p>
-
-            <motion.div 
-            
+        <div>
+          <motion.h1
             variants={container(0)}
             initial="hidden"
             animate="visible"
-            className='mx-auto w-1/2 max-w-md  m-5 flex flex-row justify-center items-center  gap-4 text-2xl'>
-              <a href='https://github.com/itsFanix' target='_blank'>
-                <FaGithub className='size-10' />
-              </a>
+            className="pb-4 text-5xl font-thin tracking-tight lg:mt-6 lg:text-6xl 2xl:text-7xl"
+          >
+            Fanny NYATSIKOR
+          </motion.h1>
+          <motion.span
+            variants={container(0)}
+            initial="hidden"
+            animate="visible"
+            className="bg-gradient-to-r from-green-300 via-blue-400 to-purple-500 bg-clip-text text-2xl tracking-tight font-thin text-transparent"
+          >
+            Sotfware Engineer - Language Enthusiast
+          </motion.span>
 
-              <a href='https://www.linkedin.com/in/gnyatsikor/' target='_blank'>
-                <FaLinkedin className='size-10' />
-              </a>
+          <motion.p
+            variants={container(0)}
+            initial="hidden"
+            animate="visible"
+            className="my-2 max-w-xl py-4 font-light tracking-tighter text-justify"
+          >
+            {ABOUT_ME}
+          </motion.p>
 
+          <motion.div
+            variants={container(0)}
+            initial="hidden"
+            animate="visible"
+            className="mx-auto w-1/2 max-w-md  m-5 flex flex-row justify-center items-center  gap-4 text-2xl"
+          >
+            <a href="https://github.com/itsFanix" target="_blank">
+              <FaGithub className="size-10" />
+            </a>
 
-            </motion.div>
-          </div>
-
+            <a href="https://www.linkedin.com/in/gnyatsikor/" target="_blank">
+              <FaLinkedin className="size-10" />
+            </a>
+          </motion.div>
         </div>
-        {/* className='w-full lg:w-1/2 lg:p-8' */}
-        <div >
-          <div className='flex justify-center'>
-
-            <motion.img
-              initial={{ x: 100, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 1, delay: 0 }}
-              src={proficPic} alt="" className='rounded-full h-80 opacity-80 ' />
-          </div>
-
+      </div>
+      {/* className='w-full lg:w-1/2 lg:p-8' */}
+      <div>
+        <div className="">
+          <motion.img
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1, delay: 0 }}
+            src={proficPic}
+            alt=""
+            className="rounded-full h-80 opacity-80 "
+          />
         </div>
-        {/* <div className=' mt-20 lg:mt-80'>
+      </div>
+      {/* <div className=' mt-20 lg:mt-80'>
           <hr className='border-t border-gray-600 border-27 border-0.5 mx-auto  max-w-5xl ' />
 
           <div className='mx-auto w-1/2 max-w-md  m-3 flex flex-row justify-center items-center  gap-4 text-2xl'>
@@ -87,10 +84,10 @@ function Home() {
             <FaInstagram />
           </div>
         </div> */}
-      </div>
+    </div>
 
     // </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
